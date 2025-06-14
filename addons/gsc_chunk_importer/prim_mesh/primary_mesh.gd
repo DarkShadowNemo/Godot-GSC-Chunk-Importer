98bd36224dbd245c8ee0c531e52bfa3f480156bf
DarkShadowNemo <119450522+DarkShadowNemo@users.noreply.github.com>
2025-06-14 15:51:33 -0700

    fix overflowing errors

--- addons/gsc_chunk_importer/prim_mesh/primary_mesh.gd
+++ addons/gsc_chunk_importer/prim_mesh/primary_mesh.gd
@@ -12,9 +12,17 @@ func _primary_mesh_(file : FileAccess, source_file : String):
 	var faces5=[]
 	var faces6=[]
 	
+	var flgidx = 0
+	
+	var taridx = 0
+	
+	var uvscales = 0.0000
+	var vcolScales = 0.0000
+	
 	var vertices : PackedVector3Array
 	var faces : PackedVector3Array
-	var normals : PackedVector3Array 
+	var normals : PackedVector3Array
+	var uvs : PackedVector2Array
 	
 	var immeediate_mesh_ := MeshInstance3D.new()
 	
@@ -52,37 +60,47 @@ func _primary_mesh_(file : FileAccess, source_file : String):
 			#var faces = [[0,1,2]]
 			
 			if flags1 == int(108):
-				for j in range(vertexCount):
-					#vertices[i] = Vector3(file.get_float(),file.get_float(),file.get_float())
-					#var nz = file.get_float()
-					var vx = file.get_float()
-					var vy = file.get_float()
-					var vz = file.get_float()
-					var type4 = file.get_8()
-					var value1 = file.get_8()
-					var normalZ = file.get_16()
-					surf_tool.add_vertex(Vector3(vx,vy,vz))
-					var type5 = type4==0
-					fa+=1
-					fb+=1
-					fc+=1
-					if type5:
-						faces4=j+j+type4-type4-1+fa-j-j-1
-						faces5=j-j+type4-type4+1+fb-2-1
-						faces6=j+type4-type4+fc-j+2-4
-						var a1=faces4
-						var b1=faces5
-						var c1=faces6
-
-						surf_tool.add_index(a1)
-						surf_tool.add_index(b1)
-						surf_tool.add_index(c1)
-						surf_tool.add_index(b1)
-						surf_tool.add_index(a1)
-						surf_tool.add_index(c1)
-				surf_tool.generate_normals(true)
+				flgidx+=1
+				
+				if flgidx <= int(255):
+					for j in range(vertexCount):
+						#vertices[i] = Vector3(file.get_float(),file.get_float(),file.get_float())
+						#var nz = file.get_float()
+						var vx = file.get_float()
+						var vy = file.get_float()
+						var vz = file.get_float()
+						var type4 = file.get_8()
+						var value1 = file.get_8()
+						var normalZ = file.get_16()
 						
+						surf_tool.add_vertex(Vector3(vx,vy,vz))
+						var type5 = type4==0
+						fa+=1
+						fb+=1
+						fc+=1
+						if type5:
+							faces4=j+j+type4-type4-1+fa-j-j-1
+							faces5=j-j+type4-type4+1+fb-2-1
+							faces6=j+type4-type4+fc-j+2-4
+							var a1=faces4
+							var b1=faces5
+							var c1=faces6
+
+							surf_tool.add_index(a1)
+							surf_tool.add_index(b1)
+							surf_tool.add_index(c1)
+							surf_tool.add_index(b1)
+							surf_tool.add_index(a1)
+							surf_tool.add_index(c1)
+											
+											
+						
+						
+				
+				surf_tool.generate_normals(true)
+				
 			array_mesh = surf_tool.commit(array_mesh)
+	
 	immeediate_mesh_.mesh = array_mesh
 	immeediate_mesh_.name = "gsc"
 	root_node.add_child(immeediate_mesh_)
